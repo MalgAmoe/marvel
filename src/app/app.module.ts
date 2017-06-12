aafe0b3d24fbea76f96d7ab0bd906080143a2460
MalgAmoe <malgamoe@gmail.com>
2017-06-12 15:15:15 +0200

    chore: created heroes-list-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { HeroesListItemComponent } from './heroes-list-item/heroes-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    HeroesListItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/heroes-list-item/heroes-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list-item',
+  templateUrl: './heroes-list-item.component.html',
+  styleUrls: ['./heroes-list-item.component.css']
+})
+export class HeroesListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
